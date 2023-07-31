e38bb3c8f10047301a31172488ecba75080d468c
Caleb Rogers <caleb@calebjay.com>
2023-07-31 17:54:46 +0800

    Update for huggingface API connection

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
 import json
+import requests
+import os
+
+API_URL = os.environ['API_URL']
+API_TOKEN = os.environ['API_TOKEN']
+headers = {"Authorization": f"Bearer {API_TOKEN}"}
+
+def query(payload):
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.json()
+
+output = query({
+	"inputs": {
+		"query": "What restaurant should I go to if I want spicy noodles?",
+		"table": {
+			"Restaurant name": ["The Ji Spot", "Mr Due Stinky Tofu", "A Normal Beef Noodle Place", "A Spicy Beef Noodle Place"],
+			"features": ["takeout only, american, chicken biscuit, fast, high quality", "taiwanese, seating, good for families, cheap", "beef noodle soup, fast, high quality, seating", "spicy, beef noodle soup, seating, clean, expensive"],
+		}
+	},
+})
+
+print(output)
+
 def main_handler(event, context):
-    return {
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return {
         "statusCode": 200,
-        "body": json.dumps('Cheers from AWS Lambda!!')
+        "body": response.json()
     }
