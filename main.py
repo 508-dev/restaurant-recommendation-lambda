6ace4b98d072764ddc945e2ba744c1a10e571632
Caleb Rogers <caleb@calebjay.com>
2023-08-02 20:25:20 +0800

    Add query handling to lambda

--- main.py
+++ main.py
@@ -7,24 +7,38 @@ API_TOKEN = os.environ['API_TOKEN']
 
 headers = {"Authorization": f"Bearer {API_TOKEN}"}
 
-def query(payload):
-	response = requests.post(API_URL, headers=headers, json=payload)
-	return response.json()
+table = {
+    "Restaurant name": ["The Ji Spot", "Mr Due Stinky Tofu", "A Normal Beef Noodle Place", "A Spicy Beef Noodle Place"],
+    "features": ["takeout only, american, chicken biscuit, fast, high quality", "taiwanese, seating, good for families, cheap", "beef noodle soup, fast, high quality, seating", "spicy, beef noodle soup, seating, clean, expensive"],
 
-output = query({
-	"inputs": {
-		"query": "What restaurant should I go to if I want spicy noodles?",
-		"table": {
-			"Restaurant name": ["The Ji Spot", "Mr Due Stinky Tofu", "A Normal Beef Noodle Place", "A Spicy Beef Noodle Place"],
-			"features": ["takeout only, american, chicken biscuit, fast, high quality", "taiwanese, seating, good for families, cheap", "beef noodle soup, fast, high quality, seating", "spicy, beef noodle soup, seating, clean, expensive"],
-		}
-	},
-})
+}
+
+def query(payload):
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.json()
+
 
 
 def lambda_handler(event,contex):
-	return {
-        "statusCode": 200,
-        'headers': {'Content-Type': 'application/json'},
-        "body": json.dumps(output)
-    }
+    operation = event['operation'];
+
+    if operation == 'query':
+        output = query({
+            "inputs": {
+                "query": event["payload"]["query"],
+                "table": table,
+            }
+        })
+
+        return {
+            "statusCode": 200,
+            'headers': {'Content-Type': 'application/json'},
+            "body": json.dumps(output)
+        }
+    else:
+        return {
+            "statusCode": 400,
+            "body": json.dumps({
+                "message": "The query was malformed."
+            })
+        }
